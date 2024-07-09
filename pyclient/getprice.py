4478b6dbb2502b40b737714f6c407fb53d2202c8
Ben <benjyc@proton.me>
2024-07-09 15:25:59 +0700

    .

--- pyclient/getprice.py
+++ pyclient/getprice.py
@@ -2,6 +2,33 @@ import asyncio
 import websockets
 import requests
 import time
+import logging
+import traceback
+
+# ws errors
+# let specificStatusCodeMappings = {
+#     '1000': 'Normal Closure',
+#     '1001': 'Going Away',
+#     '1002': 'Protocol Error',
+#     '1003': 'Unsupported Data',
+#     '1004': '(For future)',
+#     '1005': 'No Status Received',
+#     '1006': 'Abnormal Closure',
+#     '1007': 'Invalid frame payload data',
+#     '1008': 'Policy Violation',
+#     '1009': 'Message too big',
+#     '1010': 'Missing Extension',
+#     '1011': 'Internal Error',
+#     '1012': 'Service Restart',
+#     '1013': 'Try Again Later',
+#     '1014': 'Bad Gateway',
+#     '1015': 'TLS Handshake'
+# };
+
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 #baseURL = 'http://www.mcaps.com/api/v0'
 baseURL = 'http://mcaps.com/api/v0'
 
@@ -32,14 +59,13 @@ def poll():
     while True:
         #get_price('ApauDzZfW4HKKUZHqQGSF45uanz2wPUtA47XQ3eypump')
         #get_price('ApauDzZfW4HKKUZHqQGSF45uanz2wPUtA47XQ3eypump')
-        get_price('4A4VVpAmwy2ngiosmpzogLuZEhCptzDCj5UrV9JTpump')
+        get_price('9EWgLt2g1GNiEvp8gaQirrejwpcJjfzFaM4fEuiXpump')
         time.sleep(3.0)
 
 
 async def test_time():
-    uri = "ws://beta.mcaps.com/ws/time"
-    print('connect ', uri)
-    #uri = "ws://mcaps.com/pricefeed"
+    uri = "wss://stream.mcaps.com/ws/time"
+    print('connect ', uri)    
     try:
         async with websockets.connect(uri) as websocket:
             print(f"Connected to {uri}")
@@ -50,7 +76,7 @@ async def test_time():
         print(f"Failed to connect or an error occurred: {e}")
 
 async def price_feed():
-    uri = "ws://beta.mcaps.com/ws/price"
+    uri = "wss://stream.mcaps.com/ws/price"
     print('connect ', uri)
     #uri = "ws://mcaps.com/pricefeed"
     try:
@@ -61,11 +87,13 @@ async def price_feed():
                 print(f"Received message: {message}")
     except Exception as e:
         print(f"Failed to connect or an error occurred: {e}")
+        logging.error(traceback.format_exc())
+
 
 
 def stream():
-    #asyncio.run(price_feed())
-    asyncio.run(test_time())
+    asyncio.run(price_feed())
+    #asyncio.run(test_time())
 
 stream()
 #poll()
