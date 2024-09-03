5969437dde833ad34da00ac1c478899675e0368b
Ben <benjyc@proton.me>
2024-09-03 16:43:19 +0700

    .

--- pyclient/prices.py
+++ pyclient/prices.py
@@ -6,8 +6,14 @@ from datetime import datetime
 def query_ohlc():        
     return make_get_request("pump/ohlc/FZYZXyhfUhvGwpdkobJ9C725gB9yxig8x9BhQ9c5n6SU")
 
-# Example usage
+def query_last(token):        
+    return make_get_request(f"pump/lastprice/{token}")
+
+
 if __name__ == "__main__":
-    data = query_ohlc()
+    # data = query_ohlc()
+    # print(data)
+    token = "FZYZXyhfUhvGwpdkobJ9C725gB9yxig8x9BhQ9c5n6SU"
+    data = query_last(token)
     print(data)
     
