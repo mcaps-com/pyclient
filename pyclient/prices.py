883b15c7eed4ed1b903fd173d609a4ac4561700f
Ben <benjyc@proton.me>
2024-09-06 11:15:40 +0700

    .

--- pyclient/prices.py
+++ pyclient/prices.py
@@ -3,17 +3,37 @@ from tzlocal import get_localzone
 from api import make_get_request
 from datetime import datetime
 
-def query_ohlc():        
-    return make_get_request("pump/ohlc/FZYZXyhfUhvGwpdkobJ9C725gB9yxig8x9BhQ9c5n6SU")
+
+def query_ohlc(token):        
+    return make_get_request(f"pump/ohlc/{token}")
+
+def query_history(token):        
+    return make_get_request(f"pump/history/{token}")
 
 def query_last(token):        
     return make_get_request(f"pump/lastprice/{token}")
 
 
 if __name__ == "__main__":
-    # data = query_ohlc()
-    # print(data)
-    token = "FZYZXyhfUhvGwpdkobJ9C725gB9yxig8x9BhQ9c5n6SU"
+    
+    token = "9WLGaJxYLAMHL6Ge3JrLnNrpEE4snDFU32gw6zpDpump"
     data = query_last(token)
     print(data)
+
+    data = query_history(token)
+    print(data)
     
+    # print(data['price_sol'])
+    # print(data['marketcap'])
+    
+    # # Get Unix time from data and convert to a datetime object in UTC
+    # unix_time_utc = data['unix_time_utc']
+    # utc_time = datetime.fromtimestamp(unix_time_utc, tz=pytz.utc)
+
+    # # Get local timezone and current local time
+    # local_tz = get_localzone() 
+    # local_time = datetime.now(local_tz)
+
+    # # Calculate the delta
+    # delta = local_time - utc_time
+    # print(f"Delta: {delta}")
